c1fbbaede10e223f4d236dd5bd95a38c8180e6b7
Thermatix <rpgjaguar@gmail.com>
2013-07-25 13:28:38 +0100

    minor update

--- app/controllers/SessionControl.rb
+++ app/controllers/SessionControl.rb
@@ -3,7 +3,7 @@ class SessionControl < ActionController:Helpers
 	protect_from_forgery with: :exception
 	
 	def initialize 
-		if !cookies[:id]	
+		if cookies[:id]	== nil
 		cookies[:id] = { value: request.session_options[:id], expires: 1.hour.from_now}
 		end
 		#@session_id = cookies[:id]
